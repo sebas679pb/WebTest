fbdf57ae86bbec39f2c529a7fe22c04d6e87698c
unknown <jhon.pineros@labinfo.is.escuelaing.edu.co>
2022-07-19 09:29:48 -0500

    test impl

---
+++ src/test/java/co/edu/escuelaing/test/TestingWebApplicationTests.java
@@ -0,0 +1,14 @@
+package co.edu.escuelaing.test;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
